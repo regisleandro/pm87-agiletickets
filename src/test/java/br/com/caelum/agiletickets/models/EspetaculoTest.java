b60bdbea3c9374d856a9d6883d181bf15e97eaa8
Regis <regisleandro@gmail.com>
2014-07-23 17:56:26 -0300

    Implementando TDD

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -1,8 +1,15 @@
 package br.com.caelum.agiletickets.models;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import org.junit.Test;
 
 public class EspetaculoTest {
@@ -81,4 +88,60 @@ public class EspetaculoTest {
 		return sessao;
 	}
 	
+	@Test
+	public void deveCriar1SessaoParaDatasIguaisEPeridiodicidadeDiaria(){
+		LocalDate inicio = new LocalDate();
+		LocalDate fim = inicio;
+		LocalTime horario = new LocalTime();
+		Periodicidade diaria = Periodicidade.DIARIA;
+		
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> lista = espetaculo.criaSessoes(inicio, fim, horario, diaria);
+		assertEquals(1, lista.size());
+		assertEquals(inicio.toDateTime(horario), lista.get(0).getInicio());
+	}
+	
+	@Test
+	public void deveCriar7SessoesParaPeriodicidadeDiaria(){
+		LocalDate inicio = new LocalDate();
+		LocalDate fim = inicio.plusDays(7);
+		LocalTime horario = new LocalTime();
+		Periodicidade diaria = Periodicidade.DIARIA;
+		
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> lista = espetaculo.criaSessoes(inicio, fim, horario, diaria);
+		assertEquals(7, lista.size());
+		for (Sessao s : lista){
+			assertEquals(inicio.toDateTime(horario), s.getInicio());
+			inicio = inicio.plusDays(1);
+		}
+	}
+	
+	public void deveCriar1SessaoParaDatasIguaisEPeriodicidadeSemanal(){
+		LocalDate inicio = new LocalDate();
+		LocalDate fim = inicio;
+		LocalTime horario = new LocalTime();
+		Periodicidade semanal = Periodicidade.SEMANAL;
+		
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> lista = espetaculo.criaSessoes(inicio, fim, horario, semanal);
+		assertEquals(1, lista.size());
+		assertEquals(inicio.toDateTime(horario), lista.get(0).getInicio());		
+	}
+	
+	@Test
+	public void deveCriar5SessoesParaPeriodicidadeSemanal(){
+		LocalDate inicio = new LocalDate();
+		LocalDate fim = inicio.plusWeeks(5);
+		LocalTime horario = new LocalTime();
+		Periodicidade diaria = Periodicidade.SEMANAL;
+		
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> lista = espetaculo.criaSessoes(inicio, fim, horario, diaria);
+		assertEquals(5, lista.size());
+		for (Sessao s : lista){
+			assertEquals(inicio.toDateTime(horario), s.getInicio());
+			inicio = inicio.plusWeeks(1);
+		}
+	}
 }
